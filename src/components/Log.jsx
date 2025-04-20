ced68dee1f9455990bdf611a4e82aaa560a91342
Gor Nikoghosyan <gor.mainaddress@gmail.com>
2025-04-20 18:38:41 +0300

    Added styles to reset buttons in the Log

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -3,7 +3,7 @@ export default function Log({ moves, onResetTo }) {
         <ol id="log">
             {moves.map((move, index) => (
                 <li key={`${move.row}${move.col}`}>
-                    {move.symbol} selected {move.row}, {move.col}
+                    {move.symbol} selected cell [{move.row + 1}, {move.col + 1}]
                     <button onClick={() => onResetTo(index + 1)}>
                         Continue from here
                     </button>
